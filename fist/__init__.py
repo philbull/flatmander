7e660ec414ad77195ea886fa3b784d7fafef2fc8
Phil Bull <philbull@gmail.com>
2016-01-16 20:22:59 -0800

    Clean-up directory structure, rename precond.py

--- fist/__init__.py
+++ fist/__init__.py
@@ -1,8 +1,9 @@
 
-from gibbs_tools import (plot, fillWithGaussianRandomField, makeEmptyCEATemplate,
-                        writeBinnedSpectrum, addWhiteNoise, makeTemplate,
-                        applyBeam, make2dPowerSpectrum, makeMask, 
-                        litemap_from_fits,generate_gaussian_window)
+from map_tools import (plot, empty_map, beam_template, apply_beam, 
+                       white_noise_map, power_spectrum_2d, 
+                       realize_gaussian_field, 
+                       gaussian_apod_window, mask_edges, mask_disc, 
+                       litemap_from_fits, )
 
 from cluster_mcmc import (residual_maps_for_cluster, skymap_for_cluster,
                           get_cluster_profiles, set_cluster_profile_params,
@@ -13,5 +14,5 @@ from cluster_profile import ArnaudProfile
 from cluster_set import ClusterSet
 from utils import check_dir_exists, idxs_for_worker, experiment_settings
 
-import cg
+from cgsolver import ( prep_system, update_system_setup, build_cluster_maps,build_vcov_inverse, computeX0, computeB, applyMat,preCondConjugateGradientSolver )
 
--- fist/cg/__init__.py
+++
@@ -1,2 +0,0 @@
-
-from precond import ( prep_system, update_system_setup, build_cluster_maps,build_vcov_inverse, computeX0, computeB, applyMat,preCondConjugateGradientSolver )
